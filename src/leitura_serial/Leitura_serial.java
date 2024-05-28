024f52dc6d9bf3a16b8de786ee6fdef35fd4d51f
Ramon_Trigo <ramtrigo@gmail.com>
2024-05-28 10:41:44 -0300

    envio

---
+++ src/leitura_serial/Leitura_serial.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leitura_serial;
+
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+public class Leitura_serial implements SerialPortEventListener {
+SerialPort serialPort;
+    private BufferedReader input;
+    private OutputStream output;
+    private static final String PORT_NAMES[] = {
+       
+        "COM3", // Windows
+    };
+    private static final int TIME_OUT = 2000;
+    private static final int DATA_RATE = 9600;
+
+    public void initialize() {
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            for (String portName : PORT_NAMES) {
+                if (currPortId.getName().equals(portName)) {
+                    portId = currPortId;
+                    break;
+                }
+            }
+        }
+
+        if (portId == null) {
+            System.out.println("Não foi possível encontrar a porta COM.");
+            return;
+        }
+
+        try {
+            serialPort = (SerialPort) portId.open(this.getClass().getName(), TIME_OUT);
+            serialPort.setSerialPortParams(DATA_RATE, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            output = serialPort.getOutputStream();
+
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
+
+    public synchronized void close() {
+        if (serialPort != null) {
+            serialPort.removeEventListener();
+            serialPort.close();
+        }
+    }
+
+    public synchronized void serialEvent(SerialPortEvent oEvent) {
+        if (oEvent.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                String inputLine = input.readLine();
+                System.out.println(inputLine);
+            } catch (Exception e) {
+                System.err.println(e.toString());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Leitura_serial main = new Leitura_serial();
+        main.initialize();
+        System.out.println("Iniciando leitura da porta serial");
+        Thread t = new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(1000000);
+                } catch (InterruptedException ie) {
+                }
+            }
+        };
+        t.start();
+        System.out.println("Thread inicializada.");
+    }
+}
